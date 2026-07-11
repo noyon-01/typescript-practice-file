c46fe20d782a0faa7256d5a56fd9edc3870d8453
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-07-12 01:36:50 +0600

    Initialize TypeScript project with basic setup and greeting function

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+  return `Hellow ${name}, Welcome to TypeScript Learning Journey!`;
+}
+const userName = "Noyon";
+console.log(greet(userName));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string {
+  return `Hellow ${name}, Welcome to TypeScript Learning Journey!`;
+}
+
+const userName: string = "Noyon";
+console.log(greet(userName));
