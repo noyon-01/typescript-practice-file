5122dcfb6bdf51e0a72f9de4c150ea317e6833d6
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-07-13 23:28:05 +0600

    Remove unused file xcxsd.ts

--- src/unionAndany.ts
+++ src/unionAndany.ts
@@ -21,3 +21,102 @@ for (let order of orders) {
 }
 
 console.log(currentOrder);
+
+
+
+// //! 3rd Class Example code !//
+
+// let subjects: number | string = "1M";
+// console.log(subjects);
+
+// let apiRequestStatus: "pending" | "success" | "error" = "pending";
+// apiRequestStatus = "success";
+// console.log(apiRequestStatus);
+
+// let airliensSeat: "aisle" | "window" | "middle" = "aisle";
+// airliensSeat = "window";
+// console.log(airliensSeat);
+
+// const orders = ["23", "64", "34", "52"];
+// let currentOrder: any;
+
+// for (let order of orders) {
+//   if (order === "64") {
+//     currentOrder = order;
+//   }
+// }
+
+// console.log(currentOrder);
+
+// //! 3rd Class Example code !//
+
+// let subjects: number | string = "1M";
+// console.log(subjects);
+
+// let apiRequestStatus: "pending" | "success" | "error" = "pending";
+// apiRequestStatus = "success";
+// console.log(apiRequestStatus);
+
+// let airliensSeat: "aisle" | "window" | "middle" = "aisle";
+// airliensSeat = "window";
+// console.log(airliensSeat);
+
+// const orders = ["23", "64", "34", "52"];
+// let currentOrder: any;
+
+// for (let order of orders) {
+//   if (order === "64") {
+//     currentOrder = order;
+//   }
+// }
+
+// console.log(currentOrder);
+
+// //! 3rd Class Example code !//
+
+// let subjects: number | string = "1M";
+// console.log(subjects);
+
+// let apiRequestStatus: "pending" | "success" | "error" = "pending";
+// apiRequestStatus = "success";
+// console.log(apiRequestStatus);
+
+// let airliensSeat: "aisle" | "window" | "middle" = "aisle";
+// airliensSeat = "window";
+// console.log(airliensSeat);
+
+// const orders = ["23", "64", "34", "52"];
+// let currentOrder: any;
+
+// for (let order of orders) {
+//   if (order === "64") {
+//     currentOrder = order;
+//   }
+// }
+
+// console.log(currentOrder);
+
+// //! 3rd Class Example code !//
+
+// let subjects: number | string = "1M";
+// console.log(subjects);
+
+// let apiRequestStatus: "pending" | "success" | "error" = "pending";
+// apiRequestStatus = "success";
+// console.log(apiRequestStatus);
+
+// let airliensSeat: "aisle" | "window" | "middle" = "aisle";
+// airliensSeat = "window";
+// console.log(airliensSeat);
+
+// const orders = ["23", "64", "34", "52"];
+// let currentOrder: any;
+
+// for (let order of orders) {
+//   if (order === "64") {
+//     currentOrder = order;
+//   }
+// }
+
+// console.log(currentOrder);
+
