9448a2bf4645b6ced3e34ff14a3d9b1600a109ff
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-07-14 23:45:19 +0600

    Add arrow function examples to arrow_function.ts

---
+++ src/arrow_function.ts
@@ -0,0 +1,42 @@
+// const add = (a: number, b: number): number => {
+//   return a + b;
+// };
+// console.log(add(5, 10));
+
+// const sum = (num1: number, num2: number): number => {
+//   const total = num1 + num2;
+//   return total;
+// };
+// console.log(sum(12, 18));
+
+// const makeUpperCase = (text: string): string => {
+//   return text.toUpperCase();
+// };
+// console.log(makeUpperCase(`hello hablu programmer, how are you?`));
+
+// const makeLowerCase = (text: string): string => {
+//   return text.toLowerCase();
+// };
+// console.log(makeLowerCase(`SHAHADAT.DEV26@GMAIL.COM`));
+
+// const printName = (name: string): void => {
+//   console.log(name);
+// };
+
+// printName(`Shahadat Hosen Noyon`);
+// printName(`Hablu Programmer`);
+
+// const isAdult = (age: number): boolean => {
+//   return age >= 18;
+// };
+
+// console.log(isAdult(18));
+// console.log(isAdult(13));
+
+// const multiplyNumber = (n1: number, n2: number): number => {
+//   const mechine = n1 * n2;
+//   return mechine;
+// };
+
+// console.log(multiplyNumber(2, 4));
+// console.log(multiplyNumber(4, 8));
