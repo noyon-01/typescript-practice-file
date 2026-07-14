91bed6a4b8587d0e0c24842dc09ee72a0eedc6ee
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-07-15 00:21:43 +0600

    Remove example code from template_literal.ts and add array destructuring examples to array_destructuring.ts

---
+++ src/array_destructuring.ts
@@ -0,0 +1,32 @@
+// const fruits: string[] = ["apple", "orange", "banana"];
+// const [first, second, third] = fruits;
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// const color: string[] = ["red", "green", "blue", "yello"];
+// const [firstColor, , thirdColor] = color;
+
+// console.log(firstColor);
+// console.log(thirdColor);
+
+// const number: number[] = [2, 4, 5, 3, 7, 9, 6];
+// const [firstNumber, secondNumber, ...restNumber] = number;
+
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(restNumber);
+
+// const scores: number[] = [85];
+// const [math, english = 98] = scores;
+
+// console.log(math);
+// console.log(english);
+
+// let a: number = 6;
+// let b: number = 9;
+
+// [a, b] = [b, a];
+// console.log(a);
+// console.log(b);
--- src/template_literal.ts
+++ src/template_literal.ts
@@ -10,12 +10,16 @@
 // const address: string = `I live is ${country} in ${district}`;
 // console.log(address);
 
-let productPrice: number = 999;
-productPrice = 1001;
-let productQuantity: number = 5;
-productQuantity = 10;
+// let productPrice: number = 999;
+// productPrice = 1001;
+// let productQuantity: number = 5;
+// productQuantity = 10;
 
-const totalPrice: number = productPrice * productQuantity;
-console.log(
-  `each product price is ${productPrice}, your order quantity is ${productQuantity}, so your total price is ${totalPrice}`,
-);
+// const totalPrice: number = productPrice * productQuantity;
+// console.log(
+//   `each product price is ${productPrice}, your order quantity is ${productQuantity}, so your total price is ${totalPrice}`,
+// );
+
+// let score: number = 98;
+// const result: string = `${score >= 40 ? `pass` : `fail`}`;
+// console.log(result);
