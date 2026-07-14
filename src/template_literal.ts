c928b76ba653d01e184fb363b9e9cc6bcfb5f781
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-07-14 23:59:56 +0600

    Add template literal examples to template_literal.ts

---
+++ src/template_literal.ts
@@ -0,0 +1,21 @@
+// const name: string = "Shahadat Hosen Noyon";
+// let age: number = 23;
+
+// const message: string = `my name is ${name} and I am ${age} years old.`;
+// console.log(message);
+
+// const district: string = "chattogram";
+// const country: string = "Bangladesh";
+
+// const address: string = `I live is ${country} in ${district}`;
+// console.log(address);
+
+let productPrice: number = 999;
+productPrice = 1001;
+let productQuantity: number = 5;
+productQuantity = 10;
+
+const totalPrice: number = productPrice * productQuantity;
+console.log(
+  `each product price is ${productPrice}, your order quantity is ${productQuantity}, so your total price is ${totalPrice}`,
+);
