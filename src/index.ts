bad60eb7bfcc346603197d69a0428ab4461728af
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-07-14 23:00:50 +0600

    Remove example code files and add new let_and_const.ts with variable examples

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-//! 1st Class Example Code !//
-
-// function greet(name: string): string {
-//   return `Hello ${name}, Welcome to TypeScript Learning Journey!`;
-// }
-
-// const userName: string = "Noyon";
-// console.log(greet(userName));
---
+++ src/let_and_const.ts
@@ -0,0 +1,19 @@
+//! let (যা পরিবর্তন হবে)...
+// let myAge: number = 23;
+// console.log(`now my age: ${myAge}`);
+
+// myAge = 25;
+// console.log(`two years letter my age: ${myAge}`);
+
+// let phoneName: string = `readmi note 12`;
+// console.log(`now my phone name is: ${phoneName}`);
+
+// phoneName = `readmi note 15 pro max`;
+// console.log(`one year leter my phone name is: ${phoneName}`);
+
+//! const (যা কখনো পরিবর্তন হবে না)...
+// const myBirth: number = 2002;
+// console.log(`my birth date is always: ${myBirth}`);
+
+// const phoneBrand: string = "xiaomi";
+// console.log(`my readmi phone brand name is always: ${phoneBrand}`);
