317b7f4b8db10d9c7c968ef0d1a4b09b95bf0966
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-08-02 12:08:26 +0600

    literal_type practice is completed

---
+++ src/literal_type.ts
@@ -0,0 +1,17 @@
+// type Colors = "red" | "green" | "blue";
+// const c1: Colors = "red";
+// const c2: Colors = "green";
+// const c3: Colors = "blue";
+
+// console.log(c1);
+
+// type Users = "noyon" | "rafi" | "kaiser" | "munna" | "sakib";
+// const u1: Users = "munna";
+// const u2: Users = "kaiser";
+// const u3: Users = "noyon";
+
+type Methods = "GET" | "POST" | "PATCH" | "DELETE";
+const r1: Methods = "GET";
+const r2: Methods = "POST";
+const r3: Methods = "PATCH";
+const r4: Methods = "DELETE";
